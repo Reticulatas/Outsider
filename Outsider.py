eec59bf6d8d3bb5cf98c1d166f33434ca860ef8d
nfuller <nfuller@strangeloopgames.com>
2015-10-28 00:37:53 -0700

    configs

--- Outsider.py
+++ Outsider.py
@@ -8,6 +8,8 @@ import plotly.plotly as py
 from plotly.graph_objs import *
 from datetime import datetime
 from datetime import date, timedelta
+import simplejson
+import os.path
 
 #editable
 #dev_ser = '0760b4f313cc9064'   #device serial number (Fuller's nexus)
@@ -34,6 +36,9 @@ class Company:
         self.owned_shares = 0
         self.bought_value = -1.0
 
+    def log(self, msg):
+        print('\t('+self.code+') '+msg)
+
     def plot(self):
         x = range(0,max_samples%graph_density)
         y = self.prices[::graph_density]
@@ -51,8 +56,8 @@ class Company:
         if total_cost >= current_money:
             original_buy_count = buy_count
             buy_count = int(current_money / share_price)
-            print 'WARNING: ('+self.code +') Cannot buy amount wanted (' + original_buy_count + ') because not enough money, getting (' + buy_count + ') instead!'
-        print '\t('+self.code +') Calculated buy amount: ' + buy_count + ' @ ' + share_price + '/share.  Total: ' + buy_count * share_price
+            self.log('WARNING: Cannot buy amount wanted (' + str(original_buy_count) + ') because not enough money, getting (' + str(buy_count) + ') instead!')
+        self.log('Calculated buy amount: ' + str(buy_count) + ' @ ' + str(share_price) + '/share.  Total: ' + str(buy_count * share_price))
         return buy_count
 
     def is_enough_data_to_trade(self):
@@ -78,7 +83,10 @@ class Company:
                 avg += float(hist['Close'])
             return avg / short_moving
         else:'''
-        shortavg = Share(self.code).get_50day_moving_avg()
+        try:
+            shortavg = Share(self.code).get_50day_moving_avg()
+        except:
+            return -1.0
         if shortavg is None:
             return None
         return float(shortavg)
@@ -95,7 +103,10 @@ class Company:
                 avg += float(hist['Close'])
             return avg / long_moving
         else:'''
-        longavg = Share(self.code).get_200day_moving_avg()
+        try:
+            longavg = Share(self.code).get_200day_moving_avg()
+        except:
+            return -1.0
         if longavg is None:
             return None
         return float(longavg)
@@ -119,7 +130,7 @@ class Company:
         share = Share(self.code)
         price = share.get_price()
         if price is None:
-            print 'Company ' + self.code + ' gave no price'
+            self.log('Company gave no price')
             return
         self.prices.append(float(price))
         return
@@ -132,29 +143,49 @@ class Company:
         share = Share(self.code)
         avg50 = self.get_short_moving_avg()
         avg200 = self.get_long_moving_avg()
+        if avg50 == -1.0 or avg200 == -1.0:
+            self.log('Short or Long moving average cannot be obtained due to yahoo API error')
+            return 0
+        
+        current_price_u = share.get_price()
+        if current_price_u is None:
+            self.log('Current Price is None for Stock')
+            return 0
+        
+        current_price = float(current_price_u)
         if avg50 > avg200:
             #trend change, buy
-            buy_count = self.how_many_shares_to_buy(share.price)
+            buy_count = self.how_many_shares_to_buy(current_price)
             if buy_count != 0:
-                if buy(self, buy_count) == True:
+                if buy(self, buy_count, current_price) == True:
                     self.owned_shares = buy_count
-                    self.bought_value = float(share.get_price() * self.owned_shares)
+                    self.bought_value = float(current_price * self.owned_shares)
             return self.owned_shares
         return 0
 
     def check_sell(self):
         #UNCOMMENT WHEN USING ACTUAL DATA
-        #if self.owned_shares == 0:
-        #    return False
+        if self.owned_shares == 0:
+            return False
+        
         share = Share(self.code)
+
+        current_price_u = share.get_price()
+        if current_price_u is None:
+            self.log('Current Price is None for Stock')
+            return 0
+        
+        current_price = float(current_price_u)
+        
         #UNCOMMENT WHEN USING ACTUAL DATA
-        #if self.bought_value < float(share.get_price()):
-        #    return False
+        if self.bought_value < current_price:
+            return False
+        
         avg50 = self.get_short_moving_avg()
         avg200 = self.get_long_moving_avg()
         if avg50 < avg200:
             #trend change, buy
-            sell(self, self.owned_shares)
+            sell(self, self.owned_shares,current_price)
             return True
         return False
 
@@ -167,13 +198,31 @@ def LoadPreBake(file_name):
 
     for line in lines:
         if line == '':
-            return
+            return        
         tokens = line.split(',', 1)
         comp = Company(tokens[0].strip(), tokens[1].strip())       
         companies.append(comp)
     print 'Loaded in: ' + str(len(companies)) + ' companies'
     
     return
+
+def TryAddCompany(code, name):
+    comp = Company(code, name)
+    comp_stock = Share(comp.code)
+    if comp_stock is None:
+        print(comp.name + ' is not in yahoo db')
+        return False
+    price = comp_stock.get_price()
+
+    if price is None:
+        return False
+    price = float(price)
+      
+    if price < price_limit:
+        companies.append(comp)
+        print(comp.code + ',' + comp.name)
+        return True
+    return False
 
 def LoadCSV(file_name):
     file = open(file_name, 'r')
@@ -184,38 +233,83 @@ def LoadCSV(file_name):
             return
         tokens = line.split(',', 1)
 
-        comp = Company(tokens[0].strip(), tokens[1].strip())
-        comp_stock = Share(comp.code)
-        if comp_stock is None:
-            print(comp.name + ' is not in yahoo db')
-            continue
-        price = comp_stock.get_price()
-
-        if price is None:
-            continue
-        price = float(price)
-      
-        if price < price_limit:
-            companies.append(comp)
-            print(comp.code + ',' + comp.name)
+        TryAddCompany(tokens[0].strip(), tokens[1].strip())
     return
 
-def LoadOwned():
+def LoadOwned(file_name='owned_stocks.ini'):
     file = open(file_name, 'r')
     lines = file.readlines()
 
     for line in lines:
         if line == '':
             return
+        if '#' in line:
+            continue
         tokens = line.split(',', 1)
 
         code = tokens[0].strip()
         shares = int(tokens[1])
+        found = False
         for comp in companies:
             if comp.code == code:
-                print 'Loaded: ' + code + ' with ' + shares + ' shares'
-                comp.owned_shares = shares                
+                print 'Loaded: ' + code + ' with ' + str(shares) + ' shares'
+                comp.owned_shares = shares
+                found = True
 
+        if found == False:
+            print('Company owned but not in db: ' + code + ' with ' + str(shares) + ' shares')
+    return
+
+# put owned shares in file to load back later
+def DumpOwned(file_name='owned_stocks.ini'):
+    file = open(file_name, 'w')
+
+    print('Dumping owned shares to ' + file_name)
+
+    file.write('#CODE,SHARES')
+
+    dumped = 0
+    for comp in companies:
+        if comp.owned_shares != 0:
+            file.write(comp.code + ',' + comp.owned_shares + '\n')
+            dumped += 1
+
+    file.write(' ')
+    print('\tDumped: ' + str(dumped) + ' shares')
+    return
+
+def LoadConfig(file_name='config.ini'):
+    if not os.path.isfile(file_name):
+        SaveConfig(file_name)
+    
+    file = open(file_name, 'r')
+    json = file.read()
+    
+    configStruct = simplejson.loads(json)
+
+    price_limit = configStruct["price limit"];
+    current_money = configStruct["current money"];
+    max_investment_per_comp = configStruct["max investment per comp"];
+    TESTMODE = configStruct["TEST MODE"];
+
+    print 'Config Loaded.'
+    print '\tPrice Limit:\t ' + str(price_limit)
+    print '\tCurrent Money:\t ' + str(price_limit)
+    print '\tMax Investment Per Comp: ' + str(price_limit)
+    print '\tTest Mode:\t ' + str(price_limit)
+    return
+
+def SaveConfig(file_name='config.ini'):
+    configStruct = { "price limit" : price_limit,\
+                     "current money" : current_money,\
+                     "max investment per comp" : max_investment_per_comp,\
+                     "TEST MODE" : TESTMODE,\
+                }
+    json = simplejson.dumps(configStruct, indent=4)
+    file = open(file_name, 'w')
+    file.write(json)
+
+    print 'Config Saved'    
     return
 
 def gather_prices():
@@ -229,17 +323,14 @@ def check_buy_sell():
         comp.check_buy()
     return
 
-def determine_buy():
-    return
-
 def return_menu():
-    # for i in range(0,3):
-    device.click(40,40)
-    time.sleep(1)
-    device.click(40,40)
+    while not device(resourceIdMatches="com.robinhood.android:id/notification_header_btn").exists:
+        device.click(40,40)
+        time.sleep(1)
+    print 'UI returned to main menu'
     return
 
-def buy(comp, num_to_buy):
+def buy(comp, num_to_buy, price):
     if comp.is_enough_data_to_trade() == False:
         print 'Purchase of ' + comp.code + ' skipped b/c not enough data to evaluate'
         return
@@ -251,14 +342,16 @@ def buy(comp, num_to_buy):
             device(focused=True).set_text(str(num_to_buy))
             device(resourceIdMatches ="com.robinhood.android:id/review_order_btn").click()
             time.sleep(2)
-            #device.swipe(100,200,100,-100, steps=100)
+            device.swipe(100,200,100,-100, steps=100)
         else:
             print '[TEST MODE]'
-        print 'Bought ' + comp.code + ' for ' + Share(comp.code).get_price() + ' ratio: ' + str(comp.get_short_moving_avg() / comp.get_long_moving_avg())
+
+        # output result
+        print 'Bought ' + str(num_to_buy) + 'x' + comp.code + ' for ' + str(price) + ' ratio: ' + str(comp.get_short_moving_avg() / comp.get_long_moving_avg()) + ' Total: ' + str(price * num_to_buy)
     return_menu()
     return
 
-def sell(comp, num_to_sell):
+def sell(comp, num_to_sell, price):
     if search(comp.code) == True:
         if not TESTMODE:
             device(className='android.widget.ScrollView').scroll.to(textContains='SELL')
@@ -267,10 +360,10 @@ def sell(comp, num_to_sell):
             device(focused=True).set_text(str(num_to_sell))
             device(resourceIdMatches ="com.robinhood.android:id/review_order_btn").click()
             time.sleep(2)
-            #device.swipe(100,200,100,-100, steps=100)
+            device.swipe(100,200,100,-100, steps=100)
         else:
             print '[TEST MODE]'
-        print 'Sold ' + comp.code + ' for ' + Share(comp.code).get_price()
+        print 'Sold ' + str(num_to_sell) + 'x' + comp.code + ' for ' + str(price * num_to_sell)
     return_menu()
     return
 
@@ -317,9 +410,11 @@ print('Loading...')
 #LoadCSV('nasdaq.csv')                
 #LoadCSV('nyse.csv')
 LoadPreBake('pennies.csv')
+LoadOwned()
+LoadConfig()
 print('...Done Loading')
 
-if False:
+if True:
     #store all stock prices
     print '---Outsider Ready---'
     while True:
